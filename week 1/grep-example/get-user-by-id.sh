7d7d6f7a93f2ab56bed116fe1db889965da0d741
Kevin Tran Huu <kevin.tranhuu@revature.com>
2022-09-21 11:38:12 -0400

    add comments

--- week 1/grep-example/get-user-by-id.sh
+++ week 1/grep-example/get-user-by-id.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/bash # Shebang followed by the location of your shell
 
-echo "get-user-by-id.sh was invoked!";
+# ${0} refers to the name of the script
+echo "${0} was invoked!"; # while not necessary, semicolon is good practice to indicate the end of a statement
 
-echo "first provided argument: ${1}";
-
-echo "second provided argument: ${2}";
+# ${1} refers to the first argument provided
+# 	- ie: sh get-user-by-id 12 23 -- ${1} = 12, ${2} = 23
+echo "first provided argument: ${1}"; 
 
 grep "^${1}," mock_data.csv;
 
